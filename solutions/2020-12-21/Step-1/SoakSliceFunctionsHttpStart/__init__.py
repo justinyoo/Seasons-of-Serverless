3d88a1a67c8c5c689275880069d674bc72fb20f7
Aaron Roh <roharon@hufs.ac.kr>
2020-11-22 00:52:53 +0900

    Add: step1 durable functions

---
+++ solutions/2020-12-21/Step-1/SoakSliceFunctionsHttpStart/__init__.py
@@ -0,0 +1,16 @@
+# This function an HTTP starter function for Durable Functions.
+# Before running this sample, please:
+
+import logging
+
+import azure.functions as func
+import azure.durable_functions as df
+
+
+async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
+    client = df.DurableOrchestrationClient(starter)
+
+    instance_id = await client.start_new(req.route_params["functionName"], None, client_input=req.get_json())
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+
+    return client.create_check_status_response(req, instance_id)
