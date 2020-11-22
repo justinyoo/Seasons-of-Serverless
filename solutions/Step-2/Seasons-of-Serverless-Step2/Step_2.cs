bf5c3ca8389185d7eeddf5238ecfa0a33fece59c
Hongmin Kim <hooongman@outlook.kr>
2020-11-22 22:10:39 +0900

    Update Step_2.cs

--- solutions/Step-2/Seasons-of-Serverless-Step2/Step_2.cs
+++ solutions/Step-2/Seasons-of-Serverless-Step2/Step_2.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
@@ -11,28 +13,6 @@ namespace Seasons_of_Serverless_Step2
 {
     public static class Step_2
     {
-        [FunctionName("Step_2")]
-        public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] IDurableOrchestrationContext context)
-        {
-            var outputs = new List<string>();
-
-            // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("Step_2_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("Step_2_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("Step_2_Hello", "London"));
-
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
-        }
-
-        [FunctionName("Step_2_Hello")]
-        public static string SayHello([ActivityTrigger] string name, ILogger log)
-        {
-            log.LogInformation($"Saying hello to {name}.");
-            return $"Hello {name}!";
-        }
-
         [FunctionName("Step_2_HttpStart")]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
@@ -45,6 +25,60 @@ namespace Seasons_of_Serverless_Step2
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
             return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+
+        [FunctionName("Step_2_Orchestrator")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            var slicingGreenOnionResult = await context.CallActivityAsync<>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>("Step_2_Hello", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("Step_2_Hello", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("Step_2_Hello", "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [FunctionName("Slicing_GreenOnion")]
+        public static string Slicing_GreenOnion([OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                DateTime deadline = context.CurrentUtcDateTime.Add(TimeSpan.FromDays(1));
+                await context.CreateTimer(deadline, CancellationToken.None);
+                await context.CallActivityAsync("SendBillingEvent");
+            }
+            return $"Hello {name}!";
+        }
+
+        [FunctionName("Status")]
+        public static bool Status([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying hello to {name}.");
+
+            for (int i = 0; i < 10; i++)
+            {
+                DateTime deadline = context.CurrentUtcDateTime.Add(TimeSpan.FromDays(1));
+                await context.CreateTimer(deadline, CancellationToken.None);
+                await context.CallActivityAsync("SendBillingEvent");
+            }
+            return $"Hello {name}!";
+        }
+
+
+
+        public class Slicing_Result
+        {
+            public Slicing_Result()
+            {
+                completed = false;
+            }
+            public bool completed { get; set; }
         }
     }
 }
