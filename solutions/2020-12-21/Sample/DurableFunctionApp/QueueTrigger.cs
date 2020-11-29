16c83716d79cbdfff2c48571582ffed22438a4fc
Justin Yoo <justin.yoojh@gmail.com>
2020-11-29 17:50:35 +0900

    Update queue trigger to send webhook payload correctly

--- solutions/2020-12-21/Sample/DurableFunctionApp/QueueTrigger.cs
+++ solutions/2020-12-21/Sample/DurableFunctionApp/QueueTrigger.cs
@@ -1,17 +1,19 @@
 using System.Net.Http;
 using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 using Microsoft.Azure.WebJobs;
-
 using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
 
 namespace DurableFunctionApp
 {
     public static class QueueTrigger
     {
         private static HttpClient httpClient = new HttpClient();
-        private static MediaTypeFormatter formatter = new JsonMediaTypeFormatter();
+        private static JsonMediaTypeFormatter formatter = new JsonMediaTypeFormatter();
 
         [FunctionName("QueueTrigger")]
         public static async Task Run(
@@ -21,9 +23,15 @@ namespace DurableFunctionApp
             log.LogInformation($"C# Queue trigger function processed: {queue}");
 
             var payload = new ResponseModel() { Completed = true };
-            var formatter = new JsonMediaTypeFormatter();
 
-            var response = await httpClient.PostAsJsonAsync(queue, payload);
+            using (var req = new HttpRequestMessage(HttpMethod.Post, queue))
+            using (var content = new StringContent(JsonConvert.SerializeObject(payload, formatter.SerializerSettings)))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                req.Content = content;
+
+                await httpClient.SendAsync(req);
+            }
         }
     }
 }
