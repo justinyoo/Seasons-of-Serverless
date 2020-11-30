1498e4ed2f55bd6059fe587bb5bb267b0c5e7141
youjin kim <38131695+u0jin@users.noreply.github.com>
2020-11-30 09:32:23 +0900

    Add: Step 4,6,8 webhook, Logic app (#22)

--- solutions/2020-12-21/Step-4/isboiled_orchestrator/index.js
+++ solutions/2020-12-21/Step-4/isboiled_orchestrator/index.js
@@ -13,9 +13,10 @@ const df = require("durable-functions");
 const moment = require("moment");
 
 module.exports = df.orchestrator(function*(context) {
-    const boiled_wait_times = context.df.getInput()['timeToBoilInMinutes'];
+    const input = context.df.getInput();
+    //const boiled_wait_times = input.timeToBoilInMinutes;
     const boiled_wait_intervals = 60;
-    const wait_times = moment.utc(context.df.currentUtcDateTime).add(boiled_wait_times, 'm');
+    const wait_times = moment.utc(context.df.currentUtcDateTime).add(input.timeToBoilInMinutes, 'm');
     const expiryTime = moment.utc(context.df.currentUtcDateTime).add(60, 'm');
 
     context.df.setCustomStatus('{"boiled" : false}');
@@ -24,17 +25,19 @@ module.exports = df.orchestrator(function*(context) {
     while (moment.utc(context.df.currentUtcDateTime).isBefore(expiryTime)) 
     {
         const outputs = [];
-        const boiledStatus = yield context.df.callActivity("boiledstatus", "boiled");
+        const boiledStatus = yield context.df.callActivity("boiledstatus", input.callbackUrl);
         const obj = JSON.parse(boiledStatus);
 
         if (obj.boiled === true) 
         {
-            outputs.push(yield context.df.callActivity("boiledstatus", "boiled"));
+            //outputs.push(yield context.df.callActivity("boiledstatus", callback));
+            outputs.push(yield context.df.callActivity("boiledstatus", input.callbackUrl));
             context.df.setCustomStatus(outputs);
-            return outputs;
+            return true;
         }
         const nextCheck = moment.utc(context.df.currentUtcDateTime).add(boiled_wait_intervals, 's');
-        outputs.push(yield context.df.callActivity("boiledstatus", "boiled"));
+        //outputs.push(yield context.df.callActivity("boiledstatus", callback));
+        outputs.push(yield context.df.callActivity("boiledstatus", input.callbackUrl));
         context.df.setCustomStatus(outputs);
         yield context.df.createTimer(nextCheck.toDate());
     }
