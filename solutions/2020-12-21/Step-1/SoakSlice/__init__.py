ee153329922694456b38d9e844892fb6373b543b
Aaron Roh <roharon98@gmail.com>
2020-12-01 22:26:17 +0900

    Update: step[1,3,9] endpoint route, step9 images, return type (#31)

--- solutions/2020-12-21/Step-1/SoakSlice/__init__.py
+++ solutions/2020-12-21/Step-1/SoakSlice/__init__.py
@@ -11,18 +11,20 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> dict:
     bool_data = [True, False]
-    # pick_data = choice(bool_data)
-    pick_data = True
+    pick_data = choice(bool_data)
 
     if pick_data:
         # Must execute when pick_data is True
-        logging.info(req)
-        requests.post(
+        res = requests.post(
             req,
             headers={"Content-Type": "application/json"},
             data=json.dumps({
                 "completed": pick_data
             })
         )
+
+        logging.info("Callback_url: {} res: {} {}"\
+            .format(req, res.status_code, res.content))
+
 
     return pick_data
