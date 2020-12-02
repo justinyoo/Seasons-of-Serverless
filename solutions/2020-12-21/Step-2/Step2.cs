6bd6f44791115f569dbab5d667906337e24ac2ab
Justin Yoo <justin.yoojh@gmail.com>
2020-12-02 13:33:25 +0900

    Update step 2 & 7

--- solutions/2020-12-21/Step-2/Step2.cs
+++ solutions/2020-12-21/Step-2/Step2.cs
@@ -50,7 +50,8 @@ namespace Seasons_of_Serverless_Step2
                 log.LogInformation($"Start Step2_Orchestrator with 'timeToSliceValue' during : {step2_RequestData.TimeToSliceInMinutes}minute");
             }
 
-            RetryOptions retryPolicy = new RetryOptions(firstRetryInterval: TimeSpan.FromMinutes(1), maxNumberOfAttempts: step2_RequestData.TimeToSliceInMinutes);
+            // RetryOptions retryPolicy = new RetryOptions(firstRetryInterval: TimeSpan.FromMinutes(1), maxNumberOfAttempts: step2_RequestData.TimeToSliceInMinutes);
+            RetryOptions retryPolicy = new RetryOptions(firstRetryInterval: TimeSpan.FromMinutes(1), maxNumberOfAttempts: 10);
             //test
             //RetryOptions retryPolicy = new RetryOptions(firstRetryInterval: TimeSpan.FromSeconds(3), maxNumberOfAttempts: step2_RequestData.TimeToSliceInMinutes);
             
@@ -71,8 +72,8 @@ namespace Seasons_of_Serverless_Step2
 
             var random = new Random();
 
-            // var randomBool = random.Next(2) == 1;
-            var randomBool = true;
+            var randomBool = random.Next(2) == 1;
+            // var randomBool = true;
 
             if(!randomBool)
             {
